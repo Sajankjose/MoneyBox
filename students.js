b525328f65fefe061ec42c4f0bc5b96883d2c9fd
Sajankjose <sajankjose@gmail.com>
2024-11-24 11:06:42 +0530

    students.js

---
+++ students.js
@@ -0,0 +1,98 @@
+[
+    {
+        "accountNumber": 1060,
+        "name": "Erwin John Bijesh",
+        "username": "Erwin",
+        "password": "pass123",
+        "balance": 175.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1039,
+        "name": "Sarang M.A",
+        "username": "Sarang",
+        "password": "pass124",
+        "balance": 200.00,
+        "savingsGoal": 1000.00
+    },
+    {
+        "accountNumber": 1061,
+        "name": "Aadhidev K.A",
+        "username": "Aadhidev",
+        "password": "pass125",
+        "balance": 300.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1058,
+        "name": "Govardhan C Aji",
+        "username": "Govardhan",
+        "password": "pass126",
+        "balance": 650.00,
+        "savingsGoal": 1500.00
+    },
+    {
+        "accountNumber": 1062,
+        "name": "Ambika Roula",
+        "username": "Ambika",
+        "password": "pass127",
+        "balance": 400.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1053,
+        "name": "Vaiga Gireesh",
+        "username": "Vaiga",
+        "password": "pass128",
+        "balance": 720.00,
+        "savingsGoal": 1000.00
+    },
+    {
+        "accountNumber": 1063,
+        "name": "Gourinandha J",
+        "username": "Gourinandha",
+        "password": "pass129",
+        "balance": 0.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1064,
+        "name": "Parvathy",
+        "username": "Parvathy",
+        "password": "pass130",
+        "balance": 1100.00,
+        "savingsGoal": 2000.00
+    },
+    {
+        "accountNumber": 1065,
+        "name": "Adith Arun",
+        "username": "Adith",
+        "password": "pass131",
+        "balance": 70.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1066,
+        "name": "R. Adithyan",
+        "username": "Adithyan",
+        "password": "pass132",
+        "balance": 325.00,
+        "savingsGoal": 1000.00
+    },
+    {
+        "accountNumber": 1067,
+        "name": "Amar P Jithesh",
+        "username": "Amar",
+        "password": "pass133",
+        "balance": 125.00,
+        "savingsGoal": 500.00
+    },
+    {
+        "accountNumber": 1052,
+        "name": "Steeve",
+        "username": "Steeve",
+        "password": "pass134",
+        "balance": 475.00,
+        "savingsGoal": 1000.00
+    }
+]
